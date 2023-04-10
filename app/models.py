c08b8c3dcb5fc85bdd75c8b4d4fea6ed881373be
OpalescentHearts <109106306+OpalescentHearts@users.noreply.github.com>
2023-04-10 11:29:37 -0500

    Worked on getting Login/Register to load

--- app/models.py
+++ app/models.py
@@ -1,25 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 # Create your models here.
-
-class ArtistInformation(models.Model):
-    name = models.CharField(null = True, max_length=255)
-    firstName = models.CharField(max_length=200, null=True)
-    lastName = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    websiteLink = models.CharField(max_length=200, null=True)
-    bio = models.TextField(null=True, blank=True)
-    
-
-    def __str__(self):      
-        return self.name
-
-class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    picture = models.CharField(max_length=20)
-    name = models.CharField(max_length=20)
-    description = models.CharField(max_length=200) 
-    date = models.DateField()
-    price = price = models.DecimalField(decimal_places=2, max_digits=10)
--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,6 @@ from app.views import *
 urlpatterns = [
      path("admin/", admin.site.urls),
      path("", views.home, name="home"),
+     path("Login", views.LoginPage, name="Login"),
+     path("Register", views.RegisterPage, name="Register"),
 ]
