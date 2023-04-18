7dd070e859a23322473f1138197691aad0934286
OpalescentHearts <109106306+OpalescentHearts@users.noreply.github.com>
2023-04-18 13:18:01 -0500

    Organizing

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+# class CreateUserForm(UserCreationForm):
+# 	class Meta:
+# 		model = User
+# 		fields = ['username', 'password1', 'password2']           
+
+# class PostForm(ModelForm):
+#     class Meta:
+#         model = Post
+#         fields = ["picture","name",'description',"date","price"]
