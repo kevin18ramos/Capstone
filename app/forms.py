93dac3659045a02e01054f1a27065bc50ba5e029
kevin18ramos <kevin.18ramos04@gmail.com>
2023-04-18 15:11:38 -0500

    added urls views models

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,17 @@
-# class CreateUserForm(UserCreationForm):
-# 	class Meta:
-# 		model = User
-# 		fields = ['username', 'password1', 'password2']           
+from django.forms import ModelForm
+from .models import Post
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import *
 
-# class PostForm(ModelForm):
-#     class Meta:
-#         model = Post
-#         fields = ["picture","name",'description',"date","price"]
+
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username', 'password1', 'password2']           
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ["picture","name",'description',"date","price"]
--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,20 @@ from django.urls import path
 from app import views
 from app.views import *
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", views.home, name="home"),
-    path("Login/", views.login, name="login"),
-    path("Register/", views.register, name="register"),
-    path("Profile/", views.profile, name="profile"),
-]
+     path("admin/", admin.site.urls),
+     path("", views.home, name="home"),
+     path("login/", views.loginPage, name="login"),
+     path("register/", views.registerPage, name="register"),
+     path("profile", views.profile, name="profile"),
+     path("logout/", views.logoutUser, name="logout"),
+     path("products/add/", views.addProductsPage, name= "addProducts"),
+     path("products/", views.productsPage, name= "products"),
+     path("profilepage/", views.findUser, name= "findUser"),
+      path("shoppingcart", views.shoppingcart, name="shoppingcart")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
+
