0ceeb346196a6843c64220a8e89db7f3df1e4607
kevin18ramos <kevin.18ramos04@gmail.com>
2023-04-21 08:54:34 -0500

    commit before add

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 
 
 from django.db import models
@@ -19,12 +19,15 @@ class ArtistInformation(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     websiteLink = models.URLField(max_length=200, null=True)
     bio = models.TextField(null=True, blank=True)
+    cart = models.CharField(null=True)
+
+
 
     def __str__(self):
         return self.name
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture = models.ImageField(upload_to='pics/', null = True, blank = True)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200) 
