287c25f13c71ca8991be7eec0deaab00ae9e7557
coltfields23 <ccfields2003@gmail.com>
2023-04-27 16:20:46 -0500

    changed. some html

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-
-
 # Register your models here.
-
 from .models import *
-# Register your models here.
 
 admin.site.register(ArtistInformation)
 admin.site.register(Post)
