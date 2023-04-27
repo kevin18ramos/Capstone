65490836c7b4b61b64646e44197cd1c48190dc1d
Alucardrangel <bonitalight3@gmail.com>
2023-04-27 14:04:19 -0500

    Hope

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 
 from django.db import models
+
+
+
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
@@ -18,34 +21,46 @@ class ArtistInformation(models.Model):
     bio = models.TextField(null=True, blank=True)
     cart = models.CharField(max_length=200,null=True)
 
-
-
     def __str__(self):
         return self.name
 
 class Post(models.Model):
+    id = models.BigAutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to='pics/', null = True, blank = True)
+    picture = models.ImageField(upload_to='images/', null = True, blank = True)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200) 
     date = models.DateField()
+    numOfArts = models.IntegerField(default=1)
     price = price = models.DecimalField(decimal_places=2, max_digits=10)
-    # art_id = models.IntegerField(max_length=200)
     
     def __str__(self):
         return self.name
     
+class Order(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    product = models.ForeignKey(to=Post, on_delete=models.PROTECT)
+    amount = models.IntegerField()
+    customer_email = models.EmailField()
+    stripe_payment_intent = models.CharField(max_length=200)
+    has_paid = models.BooleanField(default=False)
+    
 class Cart(models.Model):
-    art = models.ForeignKey(User, on_delete=models.CASCADE)
-    TotalPrice = 0
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # art = models.ForeignKey(User, on_delete=models.CASCADE)
+    # totalprice = models.IntegerField()
+    
 
     def add_to_cart(self):
         pass
+        # if request.user.is_authenticated:
+        #     cart = Cart.objects.filter(user=request.user)
+        # cart.items.add(id)
 
-    def remove_from_cart(self):
+
+    def get_total_price(self):
         pass
 
-    def __str__(self):
-        return self.art
+
 
 
